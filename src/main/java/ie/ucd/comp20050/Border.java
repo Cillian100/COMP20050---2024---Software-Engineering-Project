e98ef27e6668f3fea526534836c3322e31c7b051
eoin <eoin.cummins@ucdconnect.ie>
2024-02-22 13:06:15 +0000

    border boilerplate class

---
+++ src/main/java/ie/ucd/comp20050/Border.java
@@ -0,0 +1,15 @@
+package ie.ucd.comp20050;
+//border class boilerplate
+public class Border {
+    public Boolean passedBorder()
+    {
+
+        return false;
+    }
+    public void setEntryPoint(int x,int y)
+    {
+
+    }
+
+
+}
