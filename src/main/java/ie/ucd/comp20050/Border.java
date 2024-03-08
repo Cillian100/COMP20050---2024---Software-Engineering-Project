fe32345029048a4bc88f14e0783e5448ff1f3b4d
Steven Kapugi <stevenkapugi@aenead.net>
2024-03-08 11:39:56 +0000

    Alleviated Border issues

--- src/main/java/ie/ucd/comp20050/Border.java
+++ src/main/java/ie/ucd/comp20050/Border.java
@@ -1,29 +1,18 @@
 package ie.ucd.comp20050;
-<<<<<<< HEAD
 
-public class Border{
+public class Border {
     int[] x;
     int[] y;
     int numberOfPoints;
-    
-    Border(int[] xray, int[] yellow, int number){
-        x=xray;
-        y=yellow;
-        numberOfPoints=number;
-    }    
-=======
-//border class boilerplate
-public class Border {
-    public Boolean passedBorder()
-    {
 
+    Border(int[] xray, int[] yellow, int number) {
+        x = xray;
+        y = yellow;
+        numberOfPoints = number;
+    }
+
+    public Boolean passedBorder() {
         return false;
     }
-    public void setEntryPoint(int x,int y)
-    {
 
-    }
-
-
->>>>>>> 9b5d0b10b3fc44ffbf35d61486cc193194b91018
 }
